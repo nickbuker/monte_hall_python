cd99e54b7f3317c9f6fc6b9da009f3eb12a268c7
Buker, Nick <Nick.Buker@nordstrom.com>
2017-09-19 08:40:26 -0700

    finish docstrings

--- src/monte_hall.py
+++ src/monte_hall.py
@@ -1,12 +1,15 @@
 import numpy as np
 
 def monte_hall(switch_door, n_sim=10000):
-    """
+    """ Simulates the Monte Hall problem to find the probability of winning
+    by either the strategy of keeping the original guess or switching doors
 
     Parameters
     ----------
-    switch_door
-    n_sim
+    switch_door : bool
+        whether or not to switch doors after being shown a door
+    n_sim : int
+        number of simulations to run
 
     Returns
     -------
@@ -17,16 +20,19 @@ def monte_hall(switch_door, n_sim=10000):
 
 
 def _run_sims(switch_door, n_sim):
-    """
+    """ Runs the simulation n_sim times and returns the mean outcome (prop games won)
 
     Parameters
     ----------
-    swich_door
-    n_sim
+    switch_door : bool
+        whether or not to switch doors after being shown a door
+    n_sim : int
+        number of simulations to run
 
     Returns
     -------
-
+    float
+        proportion of games one
     """
     doors = np.array([0, 0, 1])
     results = []
@@ -36,6 +42,22 @@ def _run_sims(switch_door, n_sim):
 
 
 def _play_game(switch_door, doors):
+    """ Runs an individual game
+
+    Parameters
+    ----------
+    switch_door : bool
+        whether or not to switch doors after being shown a door
+    doors : numpy array
+        array with 3 elements (two zeroes and a one) corresponding to
+        wins or loss
+
+    Returns
+    -------
+    int
+        0 for loss or 1 for victory in the game
+
+    """
     np.random.shuffle(doors)
     if switch_door:
         if doors[1] == 0:
